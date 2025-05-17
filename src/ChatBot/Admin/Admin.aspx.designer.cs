bef65c8211707b47a934d1950797a05df49b31e1
Joel Fuller <31699017+joelfuller2016@users.noreply.github.com>
2025-05-17 14:57:38 -0400

    Add missing designer files for WebForms pages

---
+++ src/ChatBot/Admin/Admin.aspx.designer.cs
@@ -0,0 +1,13 @@
+namespace ChatBot.Admin
+{
+    public partial class Admin
+    {
+        protected global::System.Web.UI.WebControls.TextBox ApiKey;
+        protected global::System.Web.UI.WebControls.TextBox Model;
+        protected global::System.Web.UI.WebControls.TextBox GoogleClientId;
+        protected global::System.Web.UI.WebControls.TextBox GoogleClientSecret;
+        protected global::System.Web.UI.WebControls.TextBox AdminUser;
+        protected global::System.Web.UI.WebControls.TextBox AdminPassword;
+        protected global::System.Web.UI.WebControls.Button SaveButton;
+    }
+}
